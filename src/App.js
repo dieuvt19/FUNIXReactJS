cdff7845b8abe7885b2cd34a51f5d67d3b3f519b
Dieu Dieu <vothuydieu1903@gmail.com>
2022-05-21 20:55:54 +0700

    Add asm1

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import StaffList from "./components/StaffListComponent";
+import { STAFFS } from "./shared/staffs";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffs: STAFFS,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
+          </div>
+        </Navbar>
+        <StaffList staffs={this.state.staffs} />
+      </div>
+    );
+  }
+}
+
+export default App;
