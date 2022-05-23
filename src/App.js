9cc7001451c8d28f061933a59857ca4de72ee9dd
Dieu Dieu <vothuydieu1903@gmail.com>
2022-05-23 19:52:44 +0700

    Delete style h4

--- src/App.js
+++ src/App.js
@@ -2,15 +2,7 @@ import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import StaffList from "./components/StaffListComponent";
 import { STAFFS } from "./shared/staffs";
-import styled from "styled-components";
 import "./App.css";
-
-const StaffListStyle = styled(StaffList)`
-  &:hover {
-    background-color: #e2e2e2;
-    border-radius: 3px;
-  }
-`;
 
 class App extends Component {
   constructor(props) {
@@ -28,10 +20,8 @@ class App extends Component {
             <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
           </div>
         </Navbar>
-        <StaffList staffs={this.state.staffs} />
-        <h4 className="container margin">
-          Bấm vào tên nhân viên để xem thông tin.
-        </h4>
+        <StaffList className="table-hover" staffs={this.state.staffs} />
+        <h4 className="container">Bấm vào tên nhân viên để xem thông tin.</h4>
       </div>
     );
   }
