f11460f5dc0fdb56d2d44a8fc5ef4eabf111e9e7
Dieu Dieu <vothuydieu1903@gmail.com>
2022-05-26 14:27:35 +0700

    Add header and footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
 
 class Main extends Component {
@@ -19,12 +20,8 @@ class Main extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+      <div>
+        <Header />
         <Menu
           dishes={this.state.dishes}
           onClick={(dishId) => this.onDishSelect(dishId)}
@@ -36,6 +33,7 @@ class Main extends Component {
             )[0]
           }
         />
+        <Footer />
       </div>
     );
   }
