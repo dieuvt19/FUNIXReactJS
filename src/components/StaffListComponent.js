eb3e7e6d24138328367524e2dfd9ebb0ed0871cd
Dieu Dieu <vothuydieu1903@gmail.com>
2022-06-09 00:28:33 +0700

    search function

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -15,6 +15,24 @@ function RenderStaffItem({ staff, onClick }) {
   );
 }
 
+function RenderStaffSearch({ staff, onClick }) {
+  return (
+    <React.Fragment>
+      {staff.map((item) => {
+        const { id, name, image } = item;
+        return (
+          <Card>
+            <Link to={`/nhanvien/${id}`}>
+              <CardImg width="100%" src={image} value={name} />
+              <p className="col d-flex justify-content-center m-0">{name}</p>
+            </Link>
+          </Card>
+        );
+      })}
+    </React.Fragment>
+  );
+}
+
 const StaffList = (props) => {
   console.log(props);
 
@@ -27,11 +45,45 @@ const StaffList = (props) => {
       </div>
     );
   });
+
+  const [valueSearch, setValueSearch] = useState("");
+  const searchStaff = () => {
+    if (valueSearch === "") {
+      alert("Vui lòng nhập tên nhân viên cần tìm!");
+    } else {
+      const staffSearch = props.staffs.filter((staff) =>
+        staff.name.toLowerCase().includes(valueSearch.toLowerCase())
+      );
+      console.log(staffSearch);
+      return (
+        <div key={staffSearch.id} className="col-6 col-md-4 col-lg-2">
+          <div className="m-1">
+            <RenderStaffSearch staff={staffSearch} />
+          </div>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="container">
-      <h3>Nhân Viên</h3>
+      <div className="d-flex m-2">
+        <h3 className="col-7">Nhân Viên</h3>
+        <div className="col-5">
+          <input
+            type="text"
+            placeholder="Tên nhân viên"
+            onChange={(e) => setValueSearch(e.target.value)}
+          />
+          <button className="btn btn-primary btn-xs ml-2" onClick={searchStaff}>
+            Tìm kiếm
+          </button>
+        </div>
+      </div>
       <hr />
-      <div className="row ">{listStaff}</div>
+      <div className="row">
+        {valueSearch === "" ? listStaff : searchStaff(valueSearch)}
+      </div>
     </div>
   );
 };
